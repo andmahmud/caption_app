94f787c0e731efa05215fd818ba29ce11a2f7dac
MD.MAHMUDUL HASAN <126802456+andmahmud@users.noreply.github.com>
2025-01-27 16:06:04 +0600

    Update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,38 +1,81 @@
+import 'package:flutter/material.dart';
 import 'package:caption/screens/Drawer/Drawer.dart';
 import 'package:caption/screens/Settings/SettingsPage.dart';
-import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _isSearching = false;
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Builder(
-          builder: (context) {
-            return IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(120), // Height of the AppBar
+        child: ClipPath(
+          clipper: BottomRoundedClipper(), // Custom clipper for rounded corners
+          child: AppBar(
+            leading: Builder(
+              builder: (context) {
+                return IconButton(
+                  icon: const Icon(Icons.menu),
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                );
               },
-            );
-          },
-        ),
-        title: const Center(child: Text("Enjoy the Status")),
-        backgroundColor: Colors.teal,
-        foregroundColor: Colors.white,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const SettingsPage()),
-              );
-            },
+            ),
+            title: _isSearching
+                ? TextField(
+                    controller: _searchController,
+                    autofocus: true,
+                    decoration: InputDecoration(
+                      hintText: "Search...",
+                      hintStyle: const TextStyle(color: Colors.white70),
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      icon: const Icon(Icons.search, color: Colors.white),
+                      filled: true,
+                      fillColor: Colors.white.withOpacity(0.1),
+                    ),
+                    style: const TextStyle(color: Colors.white),
+                  )
+                : const Center(child: Text("Enjoy the Status")),
+            backgroundColor: Colors.teal,
+            foregroundColor: Colors.white,
+            actions: [
+              IconButton(
+                icon: Icon(_isSearching ? Icons.close : Icons.search),
+                onPressed: () {
+                  setState(() {
+                    if (_isSearching) {
+                      _isSearching = false;
+                      _searchController.clear();
+                    } else {
+                      _isSearching = true;
+                    }
+                  });
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.settings),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SettingsPage()),
+                  );
+                },
+              ),
+            ],
           ),
-        ],
+        ),
       ),
       drawer: const DrawerScreen(),
       body: SingleChildScrollView(
@@ -45,18 +88,16 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   buildCard(
                     context,
-                    color: Colors.deepPurple,
-                    icon: Icons.psychology,
+                    image: 'assets/images/sigma.png',
                     label: "Sigma Captions",
                     routeName: '/captions',
                   ),
-                  buildCard(
-                    context,
-                    color: Colors.teal,
-                    icon: Icons.favorite,
-                    label: "Favorites",
-                    routeName: '/favorites',
-                  ),
+                  // buildCard(
+                  //   context,
+                  //   image: 'assets/images/favorites.png',
+                  //   label: "Favorites",
+                  //   routeName: '/favorites',
+                  // ),
                 ],
               ),
               const SizedBox(height: 10),
@@ -65,15 +106,13 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   buildCard(
                     context,
-                    color: const Color.fromARGB(255, 30, 92, 123),
-                    icon: Icons.sentiment_dissatisfied,
+                    image: 'assets/images/sad.png',
                     label: "Sad Captions",
                     routeName: '/sad_captions',
                   ),
                   buildCard(
                     context,
-                    color: Colors.orange,
-                    icon: Icons.emoji_emotions,
+                    image: 'assets/images/funny.png',
                     label: "Funny Captions",
                     routeName: '/funny_captions',
                   ),
@@ -85,15 +124,13 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   buildCard(
                     context,
-                    color: Colors.green,
-                    icon: Icons.lightbulb,
+                    image: 'assets/images/motivational.png',
                     label: "Motivational",
                     routeName: '/motivational_captions',
                   ),
                   buildCard(
                     context,
-                    color: Colors.blueGrey,
-                    icon: Icons.nature_people,
+                    image: 'assets/images/life.png',
                     label: "Life Captions",
                     routeName: '/life_captions',
                   ),
@@ -105,15 +142,13 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   buildCard(
                     context,
-                    color: Colors.pinkAccent,
-                    icon: Icons.favorite, // Romantic context
+                    image: 'assets/images/Romantic.png',
                     label: "Romantic Captions",
                     routeName: '/romantic_captions',
                   ),
                   buildCard(
                     context,
-                    color: Colors.lightBlueAccent,
-                    icon: Icons.group, // Friendship context
+                    image: 'assets/images/Friendship.png',
                     label: "Friendship Captions",
                     routeName: '/friendship_captions',
                   ),
@@ -125,16 +160,13 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   buildCard(
                     context,
-                    color: Colors.amber,
-                    icon: Icons.cake, // Birthday context
+                    image: 'assets/images/Birthday.png',
                     label: "Birthday Captions",
                     routeName: '/birthday_captions',
                   ),
                   buildCard(
                     context,
-                    color: Colors.redAccent,
-                    icon: Icons
-                        .heart_broken_outlined, // Updated for compatibility
+                    image: 'assets/images/Breakup.png',
                     label: "Breakup Captions",
                     routeName: '/love_failure_captions',
                   ),
@@ -147,10 +179,9 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  // Helper Method to Build Cards
+  // Helper Method to Build Cards with Image
   Widget buildCard(BuildContext context,
-      {required Color color,
-      required IconData icon,
+      {required String image,
       required String label,
       required String routeName}) {
     return GestureDetector(
@@ -162,14 +193,19 @@ class HomeScreen extends StatelessWidget {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15),
         ),
-        color: color,
+        color: Colors.teal,
         child: SizedBox(
           width: 155, // Fixed width
           height: 150, // Fixed height
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(icon, color: Colors.white, size: 40),
+              Image.asset(
+                image,
+                height: 60,
+                width: 60,
+                fit: BoxFit.contain,
+              ),
               const SizedBox(height: 10),
               Text(
                 label,
@@ -185,5 +221,24 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class BottomRoundedClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, 0);
+    path.lineTo(0, size.height - 20); // Left bottom corner
+    path.quadraticBezierTo(size.width / 2, size.height, size.width,
+        size.height - 20); // Right bottom corner
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false; // No need to reclip unless necessary
   }
 }
